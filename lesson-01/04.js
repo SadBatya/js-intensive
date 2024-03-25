c9a8f39ac77c4d54765bce6b9b0b63bf240f3237
sadbatya <sadbatya@mail.ru>
2024-03-25 19:09:27 -0400

    lesson 4

---
+++ lesson-01/04.js
@@ -0,0 +1,13 @@
+// Посчитай и выведи окончательный результат в console.log.
+
+// x += 3;
+// x *= 2;
+// x -= 4;
+// x /= 3;
+
+let x = 5;
+
+console.log(x += 3)
+console.log(x *= 2)
+console.log(x -= 4)
+console.log(x /= 3)
