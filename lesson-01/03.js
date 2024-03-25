3e9301ccf5c8b5c010acb599f64c5b8b5129a4b7
sadbatya <sadbatya@mail.ru>
2024-03-25 19:39:56 -0400

    fix lesson 3

--- lesson-01/03.js
+++ lesson-01/03.js
@@ -2,17 +2,16 @@
 // ​let number = 1;
 // и используя лишь
 // --number   number--  ++number   number++
-// Получи в консоли 1,3,3,1 
+// Получи в консоли 1,3,3,1
 
 // Нужно вызвать консоль ЧЕТЫРЕ раза
 
 // Пример:
 
-// console.log(number**); 
-// console.log(**number); 
-// console.log(number**); 
-// console.log(**number); 
-
+// console.log(number**);
+// console.log(**number);
+// console.log(number**);
+// console.log(**number);
 
 let number = 1;
 
@@ -21,4 +20,4 @@ console.log(++number)
 console.log(number--)
 console.log(--number)
 
-console.log(`${number++},${++number},${number--},${--number}`)
+console.log(`${number++}, ${++number}, ${number--}, ${--number}`)
