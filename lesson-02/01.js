8ea081e1f17517b7ac427bf9f71d7b78f4c9246c
sadbatya <sadbatya@mail.ru>
2024-04-02 12:39:29 -0400

    fix task 1

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -1,13 +1,5 @@
-// // code
-// Переделай функцию на стрелочный синтаксис
-
-// function doArrow(value) {
-//     console.log(value);
-// }
-// doArrow('Передай 5')
-
 const doArrow = (value) => {
   console.log(value)
 } 
 
-doArrow('Передай 5')
+doArrow(5)
