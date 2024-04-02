02ea547e4a95e61f33e44db5aa00eb1e4d16de5b
sadbatya <sadbatya@mail.ru>
2024-04-02 13:09:51 -0400

    fix task 4

--- lesson-02/04.js
+++ lesson-02/04.js
@@ -1,9 +1,9 @@
 const giveMeRightCondition = (value1, value2, maximum) => {
-  if (value1 === value2 & value1 <= maximum) {
-      console.log('Success!');
+  if (value1 === value2 && value1 <= maximum) {
+    console.log('Success!');
   } else {
-      console.log('Something wrong!');
+    console.log('Something wrong!');
   }
-}
+};
 
-giveMeRightCondition(101, 100, -200)
+giveMeRightCondition(300, 200, 200);
