8698a37729069515f950bb28d08fff58a616980a
sadbatya <sadbatya@mail.ru>
2024-04-04 16:20:43 -0400

    fix5

--- lesson-02/05.js
+++ lesson-02/05.js
@@ -1,8 +1,3 @@
-
-// Дан объект passport. Создай его копию и поменяй имя с 
-// "Petr" на "Ivan", и убедись в консоли, что у объекта 
-// passport имя осталось "Petr", а у твоего нового объекта "Ivan".
-
  let passport = {
     name: "Petr",
     surname: "Petrov",
